213b4f2f4a833552be16538139b21273ffcf7d14
xddxz <xddxz@qq.com>
2020-08-18 13:29:47 +0800

    再试一次

--- sword/sword.go
+++ sword/sword.go
@@ -6,9 +6,9 @@ import (
 	adminTemplate "github.com/GoAdminGroup/go-admin/template"
 	"github.com/GoAdminGroup/go-admin/template/components"
 	"github.com/GoAdminGroup/go-admin/template/types"
-	"github.com/GoAdminGroup/themes/common"
-	"github.com/GoAdminGroup/themes/sword/resource"
 	"github.com/gobuffalo/packr/v2"
+	"github.com/xddxz/ga_themes/common"
+	"github.com/xddxz/ga_themes/sword/resource"
 )
 
 type Theme struct {
